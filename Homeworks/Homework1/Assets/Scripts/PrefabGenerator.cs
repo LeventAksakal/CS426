0d9f8a7e0c2c46d36f6825b5d8e156e6192b0069
Levent Aksakal <leventaksakal56@gmail.com>
2024-10-14 23:20:22 +0300

    2.7 Adding textures && 2.8 Prefabs

---
+++ Homeworks/Homework1/Assets/Scripts/PrefabGenerator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+    public GameObject prefab; // The prefab to be spawned
+    public Vector3 spawnPosition; // The position where the prefab will be spawned
+    public Vector3 randomForceRange; // The range of random force to be applied
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Optionally, you can remove this if you don't want to spawn an object at the start
+        // SpawnObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnObject();
+        }
+    }
+
+    void SpawnObject()
+    {
+        // Instantiate the prefab at the specified position with a random rotation
+        GameObject spawnedObject = Instantiate(prefab, spawnPosition, Random.rotation);
+
+        // Apply a random force to the spawned object
+        Rigidbody rb = spawnedObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 randomForce = new Vector3(
+                Random.Range(-randomForceRange.x, randomForceRange.x),
+                Random.Range(-randomForceRange.y, randomForceRange.y),
+                Random.Range(-randomForceRange.z, randomForceRange.z)
+            );
+            rb.AddForce(randomForce, ForceMode.Impulse);
+        }
+    }
+}
